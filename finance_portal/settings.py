dc67f87a67bb14da5c495720ab80dfba3db99923
esinyon-hub <127343057+esinyon-hub@users.noreply.github.com>
2026-03-24 13:19:31 +0300

    Update settings.py

--- finance_portal/settings.py
+++ finance_portal/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'finance_portal.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': '5432',
     }
 }
 
